611f780ae302cb65ffa7b72bb8e12042935a8fee
Lalit Thapa <mlalitthapa@gmail.com>
2017-08-29 23:19:35 +0545

    extract data to visualize in chart

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
 const API_KEY = 'd88864d000e25a5d060b7732a4510bfe';
-const ROOT_URL = `https://cors-anywhere.herokuapp.com/http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
 
-    const url = `${ROOT_URL}&q=${city},us`;
+    const url = `${ROOT_URL}&q=${city}`;
     const request = axios.get(url);
 
     return {
--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,7 +1,32 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import Chart from '../components/chart';
 
 class WeatherList extends Component {
+
+    renderWeather(cityData){
+
+        const city = cityData.city;
+        const cityName = city.name;
+        const temps = cityData.list.map(weather => weather.main.temp);
+        const pressures = cityData.list.map(weather => weather.main.pressure);
+        const humidities = cityData.list.map(weather => weather.main.humidity);
+
+        return (
+            <tr key={cityName}>
+                <td>{cityName}, {city.country}</td>
+                <td>
+                    <Chart data={temps} color="orange" />
+                </td>
+                <td>
+                    <Chart data={pressures} color="green" />
+                </td>
+                <td>
+                    <Chart data={humidities} color="blue" />
+                </td>
+            </tr>
+        )
+    }
 
     render() {
         return (
@@ -14,7 +39,9 @@ class WeatherList extends Component {
                     <th>Humidity</th>
                 </tr>
                 </thead>
-                <tbody></tbody>
+                <tbody>
+                {this.props.weather.map(this.renderWeather)}
+                </tbody>
             </table>
         )
     }
