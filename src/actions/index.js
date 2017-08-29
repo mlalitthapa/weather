258813022a370158f6aafaf4965075b46fd1054a
Lalit Thapa <mlalitthapa@gmail.com>
2017-08-29 22:20:31 +0545

    api request with api, reduce response from api and weather list component

---
+++ src/actions/index.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const API_KEY = 'd88864d000e25a5d060b7732a4510bfe';
+const ROOT_URL = `https://cors-anywhere.herokuapp.com/http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+
+    const url = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    }
+
+}
