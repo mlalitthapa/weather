72c945861950561e287aeecd979f2f5cf9f9e831
Lalit Thapa <mlalitthapa@gmail.com>
2017-08-30 00:01:25 +0545

    implement google map in place of city name

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import Chart from '../components/chart';
+import GoogleMap from '../components/google_map';
 
 class WeatherList extends Component {
 
@@ -11,10 +12,13 @@ class WeatherList extends Component {
         const temps = cityData.list.map(weather => weather.main.temp);
         const pressures = cityData.list.map(weather => weather.main.pressure);
         const humidities = cityData.list.map(weather => weather.main.humidity);
+        const {lat, lon} = city.coord;
 
         return (
             <tr key={cityName}>
-                <td>{cityName}, {city.country}</td>
+                <td>
+                    <GoogleMap lat={lat} lon={lon} />
+                </td>
                 <td>
                     <Chart data={temps} color="orange" unit="k" />
                 </td>
